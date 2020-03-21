fa5982f48bbd530a512f52bf1595a60f512594ac
Anurag Sharan <34830688+sharananurag998@users.noreply.github.com>
2020-03-22 00:14:02 +0530

    Created Journal Post Model and Added Superuser

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Post(models.Model):
+    schedule = models.TextField(null=True)
+    personal_goals = models.TextField(null=True)
+    professional_goals = models.TextField(null=True)
+    motivation = models.TextField(null=True)
+    happiness = models.TextField(null=True)
+    date = models.DateField(default=datetime.date.today)
+    class Meta:
+        ordering = ['-date']
+    def __str__(self):
+        return self.date.value_to_string
